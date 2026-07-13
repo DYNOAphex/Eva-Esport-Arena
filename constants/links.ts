e9ef801af7c2085cec29b26a64e7cdf0f1f58f4d
DYNOAphex <thibaut.llorens@hotmail.com>
2026-07-13 17:17:04 +0200

    chore: add application links config

---
+++ constants/links.ts
@@ -0,0 +1,5 @@
+export const AppLinks = {
+  // Remplace cette valeur par le lien EAS/APK dès que la première build est publiée.
+  install: "",
+  repository: "https://github.com/DYNOAphex/Eva-Esport-Arena",
+} as const;

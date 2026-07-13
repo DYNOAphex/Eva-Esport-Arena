ed7a5b5e2b7913cf679ae456ce09a2a524d2e355
DYNOAphex <thibaut.llorens@hotmail.com>
2026-07-13 17:14:47 +0200

    feat: add auth stack layout

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+
+import { Theme } from "../../constants/theme";
+
+export default function AuthLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: Theme.colors.background },
+      }}
+    />
+  );
+}
